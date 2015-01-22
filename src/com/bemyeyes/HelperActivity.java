2dc1630bc6ab3e35658f68cbd38e6a3720cb5327
Jens Christian Hillerup <jens@hillerup.net>
2015-01-23 00:32:18 +0100

    initial commit

---
+++ src/com/bemyeyes/HelperActivity.java
@@ -0,0 +1,16 @@
+package com.bemyeyes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelperActivity extends Activity {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	
+	    // TODO Auto-generated method stub
+	}
+
+}
